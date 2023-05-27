7dd75554ffe8d7fbb533d43d450b776918acceea
Dexight <tupikin@sfedu.ru>
2023-05-27 18:11:05 +0300

    Add check-format method

--- ConvexHull/lib/ConvexHull.rb
+++ ConvexHull/lib/ConvexHull.rb
@@ -6,41 +6,51 @@ module ConvexHull
 
   class Error < StandardError; end
 
+  def self.check_array_format(arr)
+    unless arr.is_a?(Array) && arr.size == 2 &&
+      (
+        (arr[0].is_a?(Float) || arr[0].is_a?(Integer)) &&
+          (arr[1].is_a?(Float) || arr[1].is_a?(Integer))
+      )
+      raise SyntaxError, "Array must be only [float, float], [float, int], [int, float] or [int, int]"
+    end
+  end
+  
   def self.jarvis(* points)
+    points.each { |x| check_array_format(x) }
     n = points.length
-  if n < 3
-    return []
-  end
+    hull = []
 
-  hull = []
-
-  p0 = 0
-  for i in 1...n
-    if points[i][0] < points[p0][0]
-      p0 = i
-    end
-  end
-
-  p = p0
-  q = nil
-  begin
-    hull << points[p]
-    q = (p + 1) % n
-
-    for i in 0...n
-      orientation =
-      (points[q][1] - points[p][1]) *
-      (points[i][0] - points[q][0]) -
-      (points[q][0] - points[p][0]) *
-      (points[i][1] - points[q][1])
-      if orientation < 0
-        q = i
+    if n < 3
+      hull
+    else
+      p0 = 0
+      for i in 1...n
+        if points[i][0] < points[p0][0]
+          p0 = i
+        end
       end
+
+      p = p0
+      q = nil
+      begin
+        hull << points[p]
+        q = (p + 1) % n
+
+        for i in 0...n
+          orientation =
+          (points[q][1] - points[p][1]) *
+          (points[i][0] - points[q][0]) -
+          (points[q][0] - points[p][0]) *
+          (points[i][1] - points[q][1])
+          if orientation < 0
+            q = i
+          end
+        end
+
+        p = q
+      end while p != p0
+      hull
     end
-
-    p = q
-  end while p != p0
-
-  return hull
   end
 end
