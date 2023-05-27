c3adba1fd555bc9f58553fe98671d2133a0f9dc6
venexene <venexene@gmail.com>
2023-05-27 22:46:37 +0300

    Add Graham Algorithm

--- ConvexHull/lib/ConvexHull.rb
+++ ConvexHull/lib/ConvexHull.rb
@@ -53,4 +53,35 @@ module ConvexHull
       hull
     end
   end
+  
+  
+  def self.orientation(p, q, r)
+    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+    return 0 if val.zero?
+    return 1 if val > 0
+    return 2
+  end
+
+
+  def self.graham(* points)
+    points.each { |x| check_array_format(x) }
+  
+    min_y = points.min_by { |p| p[1] }
+
+    sorted = points.sort_by { |p| [Math.atan2(p[1]-min_y[1], p[0]-min_y[0]), p[0]**2 + p[1]**2] }
+
+    stack = []
+    stack.push(sorted[0])
+    stack.push(sorted[1])
+
+    (2..sorted.size-1).each do |i|
+      while stack.size > 1 && orientation(stack[-2], stack[-1], sorted[i]) != 2
+        stack.pop
+      end
+      stack.push(sorted[i])
+    end
+
+    return stack
+  end
+
 end
